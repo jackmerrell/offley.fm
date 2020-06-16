f28524889fe129ec7bc0c7d794bdb42f7c6fa1e3
Jack Merrell <jackmerrell@gmail.com>
2020-06-16 23:25:48 +0200

    add audio track and scrub to function

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,20 @@
-console.log('\'Allo \'Allo!');
+let DateTime = luxon.DateTime;
 
+let player = document.getElementById('player');
+let button = document.getElementById('start-button');
+
+button.addEventListener('click', () => {
+    player.play().then(() => {
+      player.pause();
+      playerTime()
+    });
+  });
+
+function playerTime(){
+
+  let duration = player.duration;
+  let progress = (((DateTime.utc().hour) + DateTime.utc().minute) % duration) / duration;
+  console.log(progress * duration);
+  player.currentTime = progress * duration;
+  player.play()
+}
