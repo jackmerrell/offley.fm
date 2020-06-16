119144dbd44e5a0eca55c29c9ebff234f19b1214
Jack Merrell <jackmerrell@gmail.com>
2020-06-16 23:47:24 +0200

    fixed scrub

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,6 +3,10 @@ let DateTime = luxon.DateTime;
 let player = document.getElementById('player');
 let button = document.getElementById('start-button');
 
+console.log(DateTime.utc().second);
+
+// console.log(DateTime.utc().hour + DateTime.utc().minute + DateTime.utc().millisecond);
+
 button.addEventListener('click', () => {
     player.play().then(() => {
       player.pause();
@@ -13,8 +17,9 @@ button.addEventListener('click', () => {
 function playerTime(){
 
   let duration = player.duration;
-  let progress = (((DateTime.utc().hour) + DateTime.utc().minute) % duration) / duration;
-  console.log(progress * duration);
-  player.currentTime = progress * duration;
+  let progress = ((((DateTime.utc().hour) + DateTime.utc().minute) + DateTime.utc().second) % duration) / duration;
+  console.log('duration = ' + duration);
+  console.log('percentage to scrub to = ' + progress * 100);
+  player.currentTime = (progress * duration);
   player.play()
 }
