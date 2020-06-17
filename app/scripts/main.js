eb54af02998bf89699f59ca5357e6469f9e30969
Jack Merrell <jackmerrell@gmail.com>
2020-06-17 23:20:49 +0200

    cleanup comments

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,23 +1,23 @@
 let DateTime = luxon.DateTime;
 
-let player = document.getElementById('player');
-let play = document.getElementById('play');
-let mute = document.getElementById('mute');
+const player = document.getElementById('player');
+const play = document.getElementById('play');
+const mute = document.getElementById('mute');
 
-let numSecondsDay = 86400;
+const numSecondsDay = 86400;
 
 play.addEventListener('click', () => {
-    player.play().then(() => {
-      player.pause();
-      playerTime()
-    });
+  player.play().then(() => {
+    player.pause();
+    playerTime()
   });
+});
 
 mute.addEventListener('click', () => {
-    player.muted = true;
-  });
+  player.muted = true;
+});
 
-function playerTime(){
+function playerTime() {
 
   // get track duration length
   let duration = player.duration;
@@ -28,17 +28,16 @@ function playerTime(){
   console.log('times the track will play in a day = ' + offset);
 
   // get current time of day in seconds
-  let currentTime = ((DateTime.utc().hour)*3600) + ((DateTime.utc().minute)*60) + (DateTime.utc().second);
+  let currentTime = ((DateTime.utc().hour) * 3600) + ((DateTime.utc().minute) * 60) + (DateTime.utc().second);
   console.log('current time = ' + currentTime);
-
 
   // get current time of day in percentage
   let getDayPercent = currentTime / numSecondsDay;
-  console.log('current time as percentage = ' + getDayPercent);
+  console.log('current time of day as percentage = ' + getDayPercent);
 
+  // get the number of times the track has played today
   let numOfTimesPlayed = Math.floor(offset * getDayPercent);
   console.log('Number of times played today = ' + numOfTimesPlayed);
-
 
   // time of track to move to in seconds
   let scrubTo = duration * ((offset * getDayPercent) - numOfTimesPlayed);
